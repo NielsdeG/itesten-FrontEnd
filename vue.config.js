c62cb2c497c1267d6407d6eb9c56bf73a3de03c0
Niels de Gier <niels.de.gier@gmail.com>
2024-03-02 10:43:51 +0100

    update target

--- vue.config.js
+++ vue.config.js
@@ -15,7 +15,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://api',
+        target: 'http://localhost:8081',
         secure: true,
       }
     },
